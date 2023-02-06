31ffe46347428ccff4924db75d424063190f273c
selstamfg <stanislav.maltyz@gmail.com>
2023-02-06 12:06:46 +0200

    update canvas Menu and Start

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,41 +6,8 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public GameObject gameOverCanvas;
-    public GameObject pipesUpdateCanvas;
-    public GameObject lifeUpdateCanvas;
-    public GameObject coinsUpdateCanvas;
-    public GameObject startSceneCanvas;
-
-    private void Start()
-    {
-       
-        CountingNotActive();
-        //Time.timeScale = 0;
-
-    }
-
-    private void Update()
-    {
-        //for (int i = 0; i < 1; i++)
-        //{ 
-        //    if (Input.GetMouseButtonDown(1))
-        //    {
-        //        Debug.Log("timerend");
-        //       startSceneCanvas.SetActive(true);
-        //       Time.timeScale = 0;
-        //    }
-        //    else
-        //    {
-        //        Time.timeScale = 1;
-        //      Debug.Log("timer");
-               
-        //    }
-
-        //}
-       
-        CountingActive();
-        startSceneCanvas.SetActive(false);
-    }
+   
+   
 
     public void  GameOver()
     {
@@ -53,19 +20,19 @@ public class GameManager : MonoBehaviour
         SceneManager.LoadScene(0);
     }
 
-    private void CountingActive()
-    {
-        pipesUpdateCanvas.SetActive(true);
-        lifeUpdateCanvas.SetActive(true);
-        coinsUpdateCanvas.SetActive(true);
-    }
+    //private void CountingActive()
+    //{
+    //    pipesUpdateCanvas.SetActive(true);
+    //    lifeUpdateCanvas.SetActive(true);
+    //    coinsUpdateCanvas.SetActive(true);
+    //}
 
-    private void CountingNotActive()
-    {
-        pipesUpdateCanvas.SetActive(false);
-        lifeUpdateCanvas.SetActive(false);
-        coinsUpdateCanvas.SetActive(false);
-    }
+    //private void CountingNotActive()
+    //{
+    //    pipesUpdateCanvas.SetActive(false);
+    //    lifeUpdateCanvas.SetActive(false);
+    //    coinsUpdateCanvas.SetActive(false);
+    //}
 
     //public void TimerLight12()
     //{
